dd34d075a44fce538e5f8954d5137f14c39f4faa
manealucacristian <luca.cmanea@gmail.com>
2022-04-18 02:16:47 -0300

    Terminando logica de ADN y generacion automatica del NFT

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -10,24 +10,68 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA{
     Counters.Counter private _idCounter;
 
     uint256 public maxSupply;
+    mapping(uint256=>uint256) public tokenDNA;
     constructor(uint256 _maxSupply) ERC721("PlatziPunks","PLPPKS"){
         maxSupply = _maxSupply;
     }
+
+
     function mint()public{
         uint256 current  = _idCounter.current();
         require(current < maxSupply,"No PlatziPunks left :(");
+        tokenDNA[current] = deterministicPseudoRandomDna(current, msg.sender);
         _safeMint(msg.sender, current);
         _idCounter.increment();
     }
+
+    function _baseURI() internal pure override returns(string memory) {
+        return"https://avataars.io/";
+    }
+    //Generaremos los parametros para modificar la apiURL
+    function _paramsURI( uint256 _dna)internal view returns(string memory){
+        string memory params;
+        {
+        params = string (abi.encodePacked(
+            "accesoriesType=", getAccesoriType(uint8(_dna)),
+            "&clotheColor=",getClothColor(uint8(_dna)),
+            "&clotheType=",getClothType(uint8(_dna)),
+            "&eyeType=",getEyeType(uint8(_dna)),
+            "&eyeBrowType=",getEyeBrowType(uint8(_dna)),
+            "&facialHairColor=",getFacialHairColor(uint8(_dna)),
+            "&facialHairType=",getFacialHairType(uint8(_dna)),
+            "&hairColor=",getHairColor(uint8(_dna)),
+            "&hatColor=",getHatColor(uint8(_dna)),
+            "&graphicType=",getGraphicType(uint8(_dna)),
+            "&mouthType=",getMouthType(uint8(_dna)),
+            "&skinColor=",getSkinColor(uint8(_dna))
+            )
+        );
+
+        }
+        //concatenamos el ultimo parametro de esta manera ya que al compilar se exedia el limite de memoria por bloque que permite solidity
+        //Cuando nos encontremos en situaciones similares una buena solucion es dividir los parametros en varios bloques y luego concatenarlos
+        return string(abi.encodePacked(params,"&topType=",getTopType(uint8(_dna))));
+    }
+
+    function imageByDna(uint256 _dna)public view returns(string memory){
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+        return string(abi.encodePacked(baseURI,"?",paramsURI));
+    }
+
+
     function tokenURI(uint256 tokenId)public view  override returns(string memory){
         require(_exists(tokenId),"ERC721 Metada URI query for nonexisten token");
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDna(dna);
         string memory jsonURI = Base64.encode(
             string (abi.encodePacked(
                     '{ "name": "PlatziPunks #"}',
                     tokenId,
                     '"", "description": "Platzi Punks are randomized Avataars stored on chain to each DApp development on platzi", "image": "',
-                    "//TODO: Calculate image URL",
+                    "",
                     '"attributtes": [{"Accesories Type": "Blank", "Clothe Color": "Red","Cloth Type":"Hoodie","Eye Type":"Close","Eye Brow Type":"Angry","Facial Hair Color":"Blonde","Facial Hair Type":"MoustacheMagnum","Hair Color":"SilverGray","Hat Color":"White","Graphic Type":"Skull","Mouth Type":"Smile","Skin Color":"light","Top Type";"LongHairMiaWallace",}]',
+                    image,               
                 '"}'   
             )
         ));
--- contracts/PlatziPunksDNA.sol
+++ contracts/PlatziPunksDNA.sol
@@ -199,67 +199,80 @@ contract PlatziPunksDNA{
         "ShortHairTheCaesar",
         "ShortHairTheCaesarSidePart"
     ];
+ //Funcion determinista y no debe ser usada en produciion
+ function deterministicPseudoRandomDna(uint256 _tokenId, address _minter) public pure returns(uint256){
+     //Un addres puede convertirse en un numero parseandolo de esta manera ya que al final de cuentas es codigo hexadecimal
+     uint256 combineParams = _tokenId + uint160(_minter);
+     //Convertimos nuestro entero en un dato represantado en bytes para generar un hash
+     bytes memory encodeParams = abi.encodePacked(combineParams);
+     //El keccak256 es una funcion hash  deterministica(facil de derivar el resultado pero dificil para derivar el origen)
+     bytes32 hashedParams = keccak256(encodeParams);
+     return uint256(hashedParams);
+ }
+ 
     uint8 constant ADN_SECTION_SIZE =2;
 
     function _getDNASection (uint256 _dna, uint256 _rightDiscard) internal pure returns(uint8) {
         return uint8((_dna % (1*10 **(_rightDiscard+ADN_SECTION_SIZE)))/(1*10 **_rightDiscard));
     }
 
-    function _getAccesoriType(uint8 _dna) public view returns(string memory){
+    function getAccesoriType(uint8 _dna) public view returns(string memory){
         uint8  dnaSection = _getDNASection(_dna, 0);
         return(_accessoriesType[dnaSection % _accessoriesType.length]) ;
     }
-        function _getClothColor(uint8 _dna) public view returns(string memory){
+        function getClothColor(uint8 _dna) public view returns(string memory){
         uint8  dnaSection = _getDNASection(_dna, 2);
         return(_clotheColor[dnaSection % _clotheColor.length]) ;
     }
 
-    function _getClothType(uint8 _dna) public view returns(string memory){
+    function getClothType(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 4);
         return(_clotheType[dnaSection % _clotheType.length]);
     }
-        function _getEyeType(uint8 _dna) public view returns(string memory){
+        function getEyeType(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 6);
         return(_eyeType[dnaSection %_eyeType.length]);
     }
-        function _getEyeBrowType(uint8 _dna) public view returns(string memory){
+        function getEyeBrowType(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 8);
         return(_eyebrowType[dnaSection % _eyebrowType.length]);
     }
-        function _getFacialHairType(uint8 _dna) public view returns(string memory){
+        function getFacialHairType(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 10);
         return(_facialHairType[dnaSection % _facialHairType.length]);
     }
-        function _getFacialHairColor(uint8 _dna) public view returns(string memory){
+        function getFacialHairColor(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 12);
         return(_facialHairColor[dnaSection % _clotheColor.length]);
     }
 
-        function _getHairColor(uint8 _dna) public view returns(string memory){
+        function getHairColor(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 14);
         return(_hairColor[dnaSection % _hairColor.length]);
     }
 
-        function _getHatColor(uint8 _dna) public view returns(string memory){
+        function getHatColor(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 16);
         return(_hatColor[dnaSection % _hatColor.length]);
     }
-        function _getGraphicType(uint8 _dna) public view returns(string memory){
+        function getGraphicType(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 18);
         return(_graphicType[dnaSection % _graphicType.length]);
     }
 
-        function _getMouthType(uint8 _dna) public view returns(string memory){
+        function getMouthType(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 20);
         return(_mouthType[dnaSection % _mouthType.length]);
     }
 
-        function _getSkinColor(uint8 _dna) public view returns(string memory){
+        function getSkinColor(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 22);
         return(_skinColor[dnaSection % _skinColor.length]);
     }
-        function _getTopType(uint8 _dna) public view returns(string memory){
+        function getTopType(uint8 _dna) public view returns(string memory){
         uint8 dnaSection = _getDNASection(_dna, 24);
         return(_topType[dnaSection % _topType.length]);
     }
+
+    //TODO: Calculate  DNA
 }
